fdbb3c47168b813c786901a5b8681b79dd275765
gitcacatyu <carlosalbertolacerdadias@gmail.com>
2024-11-05 14:04:36 -0300

    borrao

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+
+const c = (el)=>document.querySelector(el);
+const cs = (el)=>document.querySelectorAll(el);
+
+
 pizzaJson.map((item, index)=>{
-    console.log(item);
-});
+    let pizzaItem = c('.models .pizza-item').cloneNode(true);
+
+        pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+
+    c('.pizza-area').append( pizzaItem  );
+}); 
